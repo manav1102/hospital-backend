92b34626930144225521e5eb7a2a25b9eb25709d
manav1102 <mittalmanav19@gmail.com>
2025-04-16 15:19:07 +0530

    db changes

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
- 
-// dotenv.config();
- 
-// mongoose.connect(process.env.MONGO_URI, {
-// useNewUrlParser: true,
-// useUnifiedTopology: true,
-// });
- 
-// mongoose.connection.on('connected', () => console.log('Connected Backend'));
-// mongoose.connection.on('error', (err) => console.log('Connection failed with - ', err));
-
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+ 
 dotenv.config();
+ 
+mongoose.connect(process.env.MONGO_URI, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
+ 
+mongoose.connection.on('connected', () => console.log('Connected Backend'));
+mongoose.connection.on('error', (err) => console.log('Connection failed with - ', err));
 
-// Connect to MongoDB (no need for useNewUrlParser or useUnifiedTopology)
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Connected Backend'))
-  .catch((err) => console.log('Connection failed with - ', err));
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// // Connect to MongoDB (no need for useNewUrlParser or useUnifiedTopology)
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log('Connected Backend'))
+//   .catch((err) => console.log('Connection failed with - ', err));
 
