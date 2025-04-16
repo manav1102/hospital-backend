4612c471a3d743281aa869f17fb01b1f7572920c
manav1102 <mittalmanav19@gmail.com>
2025-04-16 15:30:24 +0530

    port set

--- api/index.js
+++ api/index.js
@@ -30,5 +30,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Internal Server Error" });
 });
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
